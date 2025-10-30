cd49154c7def6af69e42ce099d06d24abf58d5b4
”LOVEYOUTHREETHOUND“ <3541188450@qq.com>
2025-10-30 11:22:48 +0800

    init

---
+++ 20.valid-parentheses.py
@@ -0,0 +1,42 @@
+#
+# @lc app=leetcode.cn id=20 lang=python3
+# @lcpr version=30204
+#
+# [20] 有效的括号
+#
+
+
+# @lcpr-template-start
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# "()"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "()[]{}"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "(]"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "([])"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "([)]"\n
+# @lcpr case=end
+
+#
+
