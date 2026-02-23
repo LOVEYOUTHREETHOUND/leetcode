49e2bb64e29b68080262bdd222a6805a6bd404d9
chenmiaomiao <chenmiaomiao@192.168.0.14>
2026-02-23 10:51:43 +0800

    20250223

---
+++ 56.合并区间.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=56 lang=python3
+# @lcpr version=30400
+#
+# [56] 合并区间
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        return
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [[1,3],[2,6],[8,10],[15,18]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[1,4],[4,5]]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[4,7],[1,4]]\n
+# @lcpr case=end
+
+#
+
